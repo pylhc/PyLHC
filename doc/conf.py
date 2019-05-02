17d492bae74bc9f2a5f6bb1a32599a2e6b828612
Joschua Werner Dilly <joschua.dilly@cern.ch>
2019-05-02 13:59:00 +0200

    Added cmmnbuild-dep-manager to requiremetns

--- doc/conf.py
+++ doc/conf.py
@@ -186,6 +186,5 @@ texinfo_documents = [
 autodoc_mock_imports = ['PyQt5', 'PyQt5.QtGui', 'PyQt5.QtCore', 'PyQt5.QtWidgets',
                         "matplotlib.backends.backend_qt5agg",
                         "numpy.random",  # caused an error for unused footnote in ADDbpmerror
-                        "pjlsa.LSAClient",
                         ]
 
