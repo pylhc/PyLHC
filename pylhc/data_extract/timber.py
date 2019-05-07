678c0c0687e4e9fa82f8410817fb43e662153fb4
JoschD <joschua.dilly@gmail.com>
2019-05-07 16:05:21 +0200

    #10: Added regexp filter. (#11)
    Fixes #10
    
    * Added: Regexp filter
    * Added: Beamprocess info
    * Added: Nicer Output
    * Added: Unused pytimber functionality

---
+++ pylhc/data_extract/timber.py
@@ -0,0 +1,39 @@
+"""
+Timber
+------------
+
+Useful functionality for pytimber.
+
+:module: data_extract.timber
+:author: jdilly
+
+"""
+import logging
+
+import pytimber
+
+from utils.time_tools import AccDatetime
+
+LOG = logging.getLogger(__name__)
+
+
+def find_exact_time_for_bp(acc_time: AccDatetime) -> AccDatetime:
+    """ Finds the last entry where HX:SRMP-POW equals 123.
+
+    Which is, according to the online model KnobExtractor, the timing event for SQUEEZE or RAMP.
+    I don't think this is correct (jdilly)
+    """
+    db = pytimber.LoggingDB()
+    t1, t2 = acc_time.sub(days=1).local_string(), acc_time.local_string()
+
+    event_ts, event_val = db.get('HX:SRMP-POW', t1, t2)['HX:SRMP-POW']
+    event_ts = event_ts[event_val == 123.]
+
+    if len(event_ts) == 0:
+        raise ValueError(
+            f"No valid beamprocess found in the 24h before {acc_time.utc_string()}"
+        )
+
+    exact_time = acc_time.__class__.from_timestamp(event_ts[-1])
+    LOG.debug(f"Exact time for beamprocess found: {exact_time.utc_string()}")
+    return exact_time
--- pylhc/print_machine_settings_overview.py
+++ pylhc/print_machine_settings_overview.py
@@ -10,14 +10,18 @@ or the current settings, if no time is given.
 """
 import argparse
 import logging
-import ruamel.yaml as yaml
-import logging.config
-from utils.time_tools import AcceleratorDatetime
+import re
+from typing import Iterable
 
 from data_extract.pylsa import LSAClient
-
+from utils.logging_tools import setup_logger
+from utils.time_tools import AcceleratorDatetime
 
 LOG = logging.getLogger(__name__)
+
+
+DEFAULT_BP_RE = '^(RAMP|SQUEEZE)[_-]'
+"""str: Default regexp for interesting Beamprocesses. """
 
 
 def get_options():
@@ -28,14 +32,15 @@ def get_options():
     return args.parse_args()
 
 
-def main(time=None, knobs=(), accel='lhc'):
+def main(time: str = None, knobs: Iterable[str] = (), bp_regexp: str = DEFAULT_BP_RE, accel: str = 'lhc'):
     """
     Write an overview of Beamprocess, Optics and Knob-values at given time.
 
     Args:
-        time: UTC time as YY-mm-dd HH:MM:S.f format.
-        knobs: List of knob-names to check
-        accel: Accelerator
+        time (str): UTC time as YY-mm-dd HH:MM:S.f format.
+        knobs (list): List of knob-names to check (default: all available)
+        bp_regexp (str): Regular expression to filter beamprocesses (default: '^(RAMP|SQUEEZE)[_-]')
+        accel (str): Accelerator (default 'lhc')
 
     """
     AccDT = AcceleratorDatetime[accel]
@@ -46,52 +51,65 @@ def main(time=None, knobs=(), accel='lhc'):
 
     lsa = LSAClient()
 
-    fill_no, fill_beamprocesses = lsa.get_last_fill(acc_time, accel)
-    beamprocess, bp_start = _get_last_beamprocess(fill_beamprocesses, acc_time)
+    # get beamprocess
+    fill_no, fill_bps = lsa.find_last_fill(acc_time, accel)
+    beamprocess, bp_start = _get_last_beamprocess(fill_bps, acc_time, bp_regexp)
+    bp_info = lsa.get_beamprocess_info(beamprocess)
+
+    # get optics
     optics_table = lsa.getOpticTable(beamprocess)
-    optics, optics_start = _get_last_optics(optics_table, bp_start, acc_time)
-    trims = lsa.get_trims_at_time(beamprocess, knobs, acc_time, accel)
+    optics, optics_start = _get_last_optics(optics_table, beamprocess, bp_start, acc_time)
 
-    LOG.info("\n--- Summary -------------------------------")
-    LOG.info(f"Given Time:   {acc_time.utc_string()}")
-    LOG.info(f"Fill:         {fill_no:d}")
-    LOG.info(f"Beamprocess:  {beamprocess}")
-    LOG.info(f"BP-Start:     {bp_start.utc_string()}")
-    LOG.info(f"Optics:       {optics}")
-    LOG.info(f"Optics-Start: {optics_start.utc_string()}")
+    # get trims
+    trims = lsa.find_trims_at_time(beamprocess, knobs, acc_time, accel)
+
+    # print summary
+    summary = (
+        "\n----------- Summary ---------------------\n"
+        f"Given Time:   {acc_time.utc_string()}\n"
+        f"Fill:         {fill_no:d}\n"
+        f"Beamprocess:  {beamprocess}\n"
+        f"  Start:      {bp_start.utc_string()}\n"
+        f"  Context:    {bp_info['contextCategory']}\n"
+        f"  Descr.:     {bp_info['description']}\n"
+        f"Optics:       {optics}\n"
+        f"  Start:      {optics_start.utc_string()}\n"
+        "-----------------------------------------\n"
+    )
     for trim, value in trims.items():
-        LOG.info(f"{trim:30s}: {value:g}")
+        summary += f"{trim:30s}: {value:g}\n"
+    summary += "-----------------------------------------\n\n"
+    LOG.info(summary)
 
 
-def _get_last_beamprocess(beamprocesses, acc_time):
+def _get_last_beamprocess(bps, acc_time, regexp=''):
     """ Get the last beamprocess in the list of beamprocesses before dt_utc.
     Also returns the start time of the beam-process in utc.
     """
     ts = acc_time.timestamp()
-    for time, name in reversed(beamprocesses):
-        if time <= ts:
+    LOG.debug(f"Looking for beamprocesses before '{acc_time.utc_string()}', matching '{regexp}'")
+    time, name = None, None
+    reg = re.compile(regexp, re.IGNORECASE)
+    for time, name in reversed(bps):
+        if time <= ts and reg.search(name) is not None:
             break
+    if time is None:
+        raise ValueError(f"No relevant beamprocess found in the fill before {acc_time.utc_string()}")
     return name, acc_time.__class__.from_timestamp(time)
 
 
-def _get_last_optics(optics_table, bp_start, acc_time):
+def _get_last_optics(optics_table, bp, bp_start, acc_time):
     """ Get the name of the optics at the right time for current beam process. """
     ts = acc_time.timestamp() - bp_start.timestamp()
+    item = None
     for item in reversed(optics_table):
         if item.time <= ts:
             break
+    if item is None:
+        raise ValueError(f"No optics found for beamprocess {bp}")
     return item.name, acc_time.__class__.from_timestamp(item.time + bp_start.timestamp())
 
 
-def _setup_logger():
-    """ Setup logging.
-
-    Might be replaced in the future with logging_tools.
-    """
-    with open('logging_config.yml') as stream:
-        logging.config.dictConfig(yaml.safe_load(stream))
-
-
 if __name__ == '__main__':
-    _setup_logger()
+    setup_logger()
     main(**get_options().__dict__)
