f72446e582c521824cd0cee0e359e27ed764b23d
JoschD <joschua.dilly@gmail.com>
2019-06-24 11:17:45 +0200

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="pylhc",
-    version="0.0.1",
+    version="0.0.2",
     description="Useful tools in particular for accelerator physicists at CERN",
     long_description=README,
     long_description_content_type="text/markdown",
