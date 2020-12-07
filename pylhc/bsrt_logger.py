c90907526e3bb8fc495d7798628ff65d921b31f3
JoschD <joschua.dilly@gmail.com>
2020-12-07 13:34:42 +0100

    #40 bsrt logger in if

--- pylhc/bsrt_logger.py
+++ pylhc/bsrt_logger.py
@@ -58,51 +58,50 @@ def convert_to_data_output_format(dtobject):
 ##########################################
 
 
-# Create a PyJapc instance with selector SCT.USER.ALL
-# INCA is automatically configured based on the timing domain you specify here
-# japc = pyjapc.PyJapc(selector="LHC.USER.ALL", incaAcceleratorName="LHC", noSet=True )
+if __name__ == '__main__':
+    # Create a PyJapc instance with selector SCT.USER.ALL
+    # INCA is automatically configured based on the timing domain you specify here
 
-CycleName = "LHC.USER.ALL"
-INCAacc = "LHC"
-noSetFlag = True
+    CycleName = "LHC.USER.ALL"
+    INCAacc = "LHC"
+    noSetFlag = True
 
-japc = pyjapc.PyJapc(selector=CycleName, incaAcceleratorName=INCAacc, noSet=noSetFlag)
-japc = pyjapc.PyJapc(selector=CycleName, noSet=noSetFlag)
-japc.rbacLogin()
-acquesitions_per_file = 100
-j = 0
-t = 0
-while True:
-    time.sleep(0.7)
-    print(j, t)
-    B1_image = japc.getParam("LHC.BSRTS.5R4.B1/Image")
-    B2_image = japc.getParam("LHC.BSRTS.5L4.B2/Image")
-    if t == 0:
-        allB1data = []
-        allB2data = []
-        B1_IMGtime = B1_image["acqTime"]
-        B2_IMGtime = B2_image["acqTime"]
-        B1_IMGtime_dt = parse_timestamp(B1_IMGtime)
-        B2_IMGtime_dt = parse_timestamp(B2_IMGtime)
-        B1_IMGtime_st = convert_to_data_output_format(B1_IMGtime_dt)
-        B2_IMGtime_st = convert_to_data_output_format(B2_IMGtime_dt)
+    japc = pyjapc.PyJapc(selector=CycleName, incaAcceleratorName=INCAacc, noSet=noSetFlag)
+    japc.rbacLogin()
+    acquesitions_per_file = 100
+    j = 0
+    t = 0
+    while True:
+        time.sleep(0.7)
+        print(j, t)
+        B1_image = japc.getParam("LHC.BSRTS.5R4.B1/Image")
+        B2_image = japc.getParam("LHC.BSRTS.5L4.B2/Image")
+        if t == 0:
+            allB1data = []
+            allB2data = []
+            B1_IMGtime = B1_image["acqTime"]
+            B2_IMGtime = B2_image["acqTime"]
+            B1_IMGtime_dt = parse_timestamp(B1_IMGtime)
+            B2_IMGtime_dt = parse_timestamp(B2_IMGtime)
+            B1_IMGtime_st = convert_to_data_output_format(B1_IMGtime_dt)
+            B2_IMGtime_st = convert_to_data_output_format(B2_IMGtime_dt)
 
-    allB1data.append(B1_image)
-    allB2data.append(B2_image)
-    t += 1
-    if t == acquesitions_per_file:
-        j += 1
-        f1name = "data_BSRT_B1_" + B1_IMGtime_st + ".dat"
-        f2name = "data_BSRT_B2_" + B2_IMGtime_st + ".dat"
-        f1 = open(f1name, "wb")
-        f2 = open(f2name, "wb")
-        pickle.dump(allB1data, f1)
-        pickle.dump(allB2data, f2)
-        f1.close()
-        f2.close()
-        os.system("gzip " + f1name)
-        os.system("gzip " + f2name)
-        t = 0
+        allB1data.append(B1_image)
+        allB2data.append(B2_image)
+        t += 1
+        if t == acquesitions_per_file:
+            j += 1
+            f1name = "data_BSRT_B1_" + B1_IMGtime_st + ".dat"
+            f2name = "data_BSRT_B2_" + B2_IMGtime_st + ".dat"
+            f1 = open(f1name, "wb")
+            f2 = open(f2name, "wb")
+            pickle.dump(allB1data, f1)
+            pickle.dump(allB2data, f2)
+            f1.close()
+            f2.close()
+            os.system("gzip " + f1name)
+            os.system("gzip " + f2name)
+            t = 0
 
-# Close the RBAC session
-japc.rbacLogout()  # this code in not reachable! Encapsulate in a try..except maybe?
+    # Close the RBAC session
+    japc.rbacLogout()  # this code in not reachable! Encapsulate in a try..except maybe?
