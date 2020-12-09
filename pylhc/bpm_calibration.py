c72518f9e4a2b8b7127f07db478e05b88f3a2696
Maël Le Garrec <mael@legarrec.org>
2020-12-09 17:13:59 +0100

    refactor some more

--- pylhc/bpm_calibration.py
+++ pylhc/bpm_calibration.py
@@ -111,58 +111,6 @@ def _get_params() -> dict:
     )
 
 
-def _write_calibration_tfs(
-    calibration_factors: pd.DataFrame, plane: str, method: str, outputdir: Path
-) -> None:
-    """
-    This function saves to a file the calibration factors given as input.
-    The file name contains the method and plane related to those factors.
-    e.g: "calibration_beta_x.tfs"
-
-    Args:
-      calibration_factors (pd.DataFrame): The DataFrame containing the
-        calibration factors to be written to disk.
-      plane (str): The plane associated to the current calibration factors.
-      method (str): The method used to compute those calibration factors.
-      outputdir (Path): The directory where to save thedir.
-
-    Returns:
-      None
-    """
-    # Create the output directory
-    outputdir.mkdir(parents=True, exist_ok=True)
-
-    # Reset the index of the dataframe, it was handy to handle the data but not
-    # to store it
-    calibration_factors = calibration_factors.reset_index()
-
-    # Write the TFS files for this plane
-    # The method chosen will change the tfs name
-    tfs_name = f"{CALIBRATION_NAME[method]}{plane.lower()}{EXT}"
-    file_path = outputdir / tfs_name
-    LOG.info(f"Writing {file_path}")
-    tfs.write_tfs(file_path, calibration_factors, save_index=False)
-
-
-def _get_str_calibration_factors(calibration_factors: Dict[str, pd.DataFrame]) -> str:
-    """
-    Return the calibration factor in a console readable format
-
-    Args:
-        calibration_factors (Dict[str, pd.DataFrame]): the dataframe containing
-        all the calibration factors for each plane
-
-    Returns:
-        str: The calibration factors as string
-    """
-    result_str = ""
-    for plane in calibration_factors.keys():
-        result_str += f"\nPlane {plane}:\n"
-        result_str += f"{calibration_factors[plane]}"
-
-    return result_str
-
-
 @entrypoint(_get_params(), strict=True)
 def main(opt):
     # Compute the calibration factors and their errors according to the method
@@ -174,11 +122,14 @@ def main(opt):
     # Fill NaN with 1 because of missing BPMs and that fit cannot be done everywhere
     for plane in factors.keys():
         factors[plane].fillna(1, inplace=True)
-    LOG.debug(_get_str_calibration_factors(factors))
+    LOG.debug("".join([f"\nPlane {plane}:\n{factors[plane]}" for plane in factors.keys()]))
 
     # Write the TFS file to the desired output directory
+    opt.outputdir.mkdir(parents=True, exist_ok=True)
     for plane in factors.keys():
-        _write_calibration_tfs(factors[plane], plane, opt.method, opt.outputdir)
+        tfs.write(opt.outputdir / f"{CALIBRATION_NAME[opt.method]}{plane.lower()}{EXT}", 
+                  factors[plane].reset_index(), 
+                  save_index=False)
 
     return factors
 
