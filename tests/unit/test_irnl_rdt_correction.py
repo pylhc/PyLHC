4a71ae1bae5eb7c7271fa8d99f6953e217c56b6b
JoschD <26184899+JoschD@users.noreply.github.com>
2021-10-20 17:09:49 +0200

    Soon to be a unit test

--- tests/unit/test_irnl_rdt_correction.py
+++ tests/unit/test_irnl_rdt_correction.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pandas as pd
 import tfs
 
-from pylhc.irnl_rdt_correction import main as irnl_correct, ORDER_NAME_MAP
+from pylhc.irnl_rdt_correction import main as irnl_correct, ORDER_NAME_MAP, BETA, KEYWORD, X, Y, MULTIPOLE
 
 INPUTS = Path(__file__).parent.parent / "inputs"
 LHC_MODELS_PATH = INPUTS / "model_lhc_thin_30cm"
@@ -22,22 +22,48 @@ LHC_MODELS_PATH = INPUTS / "model_lhc_thin_30cm"
 # )
 #
 
-def test_lhc_correction(tmp_path, beam, order, feeddown):
-    optics = read_lhc_model(beam)
-    errors = pd.DataFrame(["MQX."])
 
-    irnl_correct(
+
+def test_correction(tmp_path):
+    optics = generate_pseudo_model()
+    errors = generate_empty_errortable()
+    errors["K3L"] = 1
+
+    _, df_corrections = irnl_correct(
         accel='lhc',
-        optics=optics,
-        errors=,
-        beams=[beam],
+        optics=[optics],
+        errors=[errors],
+        beams=[1],
         output=tmp_path,
-        feddown=feeddown,
-        ips=[1,5],
+        feddown=0,
+        ips=[1, 5],
         ignore_missing_columns=True,
         iterations=1,
     )
 
+    pass
+
 
 def read_lhc_model(beam):
    return tfs.read_tfs(LHC_MODELS_PATH / f"twiss.lhc.b{beam}.nominal.tfs", index="NAME")
+
+
+def generate_pseudo_model(betax=1, betay=1, x=0, y=0):
+    df = pd.DataFrame(
+        index=[f"{name}.{number}{side}{ip}" for ip in range(1, 4) for side in "LR" for name, number in zip("ABC", range(1, 4)) ],
+        columns=[f"{BETA}{X}", f"{BETA}{Y}", X, Y, KEYWORD]
+    )
+    df[f"{BETA}{X}"] = betax
+    df[f"{BETA}{Y}"] = betay
+    df[X] = x
+    df[Y] = y
+    df[KEYWORD] = MULTIPOLE
+    return df
+
+
+def generate_empty_errortable():
+    df = pd.DataFrame(0,
+                      index=[f"{name}.{number}{side}{ip}" for ip in range(1, 4) for side in "LR" for name, number in zip("ABC", range(1, 4)) ],
+                      columns=[f"K{n}{o}L" for n in range(6) for o in ("", "S")]
+                      )
+    return df
