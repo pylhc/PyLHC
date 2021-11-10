ec13ececd5d74c0b005e3fe0fd726ea21a004973
JoschD <26184899+JoschD@users.noreply.github.com>
2021-11-10 17:51:11 +0100

    updated doc

--- pylhc/irnl_rdt_correction.py
+++ pylhc/irnl_rdt_correction.py
@@ -284,7 +284,7 @@ def get_parser() -> argparse.ArgumentParser:
         "--optics",
         dest="optics",
         nargs="+",
-        help="Path(s) to optics file(s).",
+        help="Path(s) to optics file(s). Defines which elements to correct for!",
         required=True,
     )
     parser.add_argument(
@@ -306,7 +306,7 @@ def get_parser() -> argparse.ArgumentParser:
         "--output",
         dest="output",
         help=("Path to write command and tfs_df file. "
-              f"Extension is ignored and replaced with {EXT_TFS} and {EXT_MADX} "
+              f"Extension (if given) is ignored and replaced with {EXT_TFS} and {EXT_MADX} "
               "for the Dataframe and the command file respectively."
               ),
     )
@@ -315,7 +315,7 @@ def get_parser() -> argparse.ArgumentParser:
         dest="rdts",
         nargs="+",
         help=("RDTs to correct."
-              " Format: 'Fjklm', or starting with '*'"
+              " Format: 'Fjklm'; or 'Fjklm*'"
               " to correct for this RDT in Beam 2 using"
               " beta-symmetry (jk <-> lm)."),
     )
@@ -386,7 +386,7 @@ def get_parser() -> argparse.ArgumentParser:
         "--ignore_missing_columns",
         dest="ignore_missing_columns",
         help=("If set, missing strength columns in any of the input files "
-              "are assumed to be zero, instead of crashing."),
+              "are assumed to be zero, instead of raising an error."),
         action="store_true",
     )
     return parser
@@ -397,34 +397,64 @@ def main(**opt) -> Tuple[str, tfs.TfsDataFrame]:
 
     Keyword Args:
 
-        optics (list[str/Path/DataFrame]): Path(s) to optics file(s) or DataFrame(s) of optics
+        optics (list[str/Path/DataFrame]): Path(s) to optics file(s) or DataFrame(s) of optics.
+                                           Needs to contain only the elements to be corrected
+                                           (e.g. only the ones in the IRs).
+                                           All elements from the error-files need to be present.
+                                           Required!
         errors (list[str/Path/DataFrame]): Path(s) to error file(s) or DataFrame(s) of errors.
-                                           Needs to contain only the involved elements (e.g. only the ones in the IRs).
+                                           Can contain less elements than the optics files,
+                                           these elements are then assumed to have no errors.
+                                           Required!
         beams (list[int]): Which beam the files come from (1, 2 or 4).
+                           Required!
         output (str/Path): Path to write command and tfs_df file.
-                           Extension is ignored and replaced with '.tfs' and '.madx'
+                           Extension (if given) is ignored and replaced with '.tfs' and '.madx'
                            for the Dataframe and the command file respectively.
-        rdts (list[str]): RDTs to correct.
-                          Format: 'Fjklm', or starting with 'J' (skew) or 'K' (normal)
-                          to force corrector type.
-        rdts2 (list[str]): RDTs to correct for second beam/file, if different from first.
-                           Same format rules as for `rdts`.
-        accel (str): Which accelerator we have. (lhc, hllhc)
-        feeddown (int):  Order of Feeddown to include.
-        ips (list[int]): In which IPs to correct
-        solver (str): Solver to use. One of 'lstsq', 'inv' or 'linear'
+                           Default: ``None``.
+        rdts (list[str], dict[str, list[str]):
+                          RDTs to correct.
+                          Format: 'Fjklm'; or 'Fjklm*' to correct for
+                          this RDT in Beam 2 using beta-symmetry (jk <-> lm).
+                          The RDTs can be either given as a list, then the appropriate correctors
+                          are determined by jklmn.
+                          Alternatively, the input can be a dictionary,
+                          where the keys are the RDT names as before, and the values are a list
+                          of correctors to use, e.g. 'b5' for normal decapole corrector,
+                          'a3' for skew sextupole, etc.
+                          If the order of the corrector is higher than the order of the RDT,
+                          the feed-down from the corrector is used for correction.
+                          In the case where multiple orders of correctors are used,
+                          increasing ``iterations`` might be useful.
+                          Default: Standard RDTs for given ``accel`` (see ``DEFAULT_RDTS`` in this file).
+        rdts2 (list[str], dict[str, list[str]):
+                           RDTs to correct for second beam/file, if different from first.
+                           Same format rules as for ``rdts``. Default: ``None``.
+        accel (str): Which accelerator we have. One of 'lhc', 'hllhc'.
+                     Default: ``lhc``.
+        feeddown (int): Order of Feeddown to include.
+                        Default: ``0``.
+        ips (list[int]): In which IPs to correct.
+                         Default: ``[1, 2, 5, 8]``.
+        solver (str): Solver to use. One of 'lstsq', 'inv' or 'linear'.
+                      Default ``lstsq``.
         update_optics (bool): Sorts the RDTs to start with the highest order
                               and updates the corrector strengths in the optics
                               after calculation, so the feeddown to lower order
                               correctors is included.
+                              Default: ``True``.
         ignore_corrector_settings (bool): Ignore the current settings of the correctors.
                                           If this is not set the corrector values of the
                                           optics are used as initial conditions.
+                                          Default: ``False``.
         ignore_missing_columns (bool): If set, missing strength columns in any
                                        of the input files are assumed to be
-                                       zero, instead of crashing.
+                                       zero, instead of raising an error.
+                                       Default: ``False``.
         iterations (int): Reiterate correction, starting with the previously
                           calculated values.
+                          Default: ``1``.
+
 
     Returns:
 
