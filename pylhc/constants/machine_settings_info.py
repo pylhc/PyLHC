d40f5161651d908ac6f6fe140ef51716abb21239
JoschD <26184899+JoschD@users.noreply.github.com>
2022-02-16 15:16:13 +0000

    Lsa with timerange (#92)
    * added trim history
    * return as dict
    * Unified CamelCase for TFS headers
    * updated doc
    * updated version

--- pylhc/constants/machine_settings_info.py
+++ pylhc/constants/machine_settings_info.py
@@ -11,17 +11,24 @@ from pylhc.constants.general import TFS_SUFFIX
 # Filename
 info_name = f"machine_settings{TFS_SUFFIX}"
 knobdef_suffix = f"_definition{TFS_SUFFIX}"
+trimhistory_suffix = f"_trims{TFS_SUFFIX}"
 
 # Columns
 column_knob = "KNOB"
+column_time = "TIME"
+column_timestamp = "TIMESTAMP"
 column_value = "VALUE"
 
+
 # Headers
+head_accel = "Accelerator"
 head_time = "Time"
+head_start_time = "StartTime"
+head_end_time = "EndTime"
 head_beamprocess = "Beamprocess"
 head_fill = "Fill"
-head_beamprocess_start = "BP_Start"
+head_beamprocess_start = "BeamprocessStart"
 head_context_category = "ContextCategory"
 head_beamprcess_description = "Description"
 head_optics = "Optics"
-head_optics_start = "Optics_Start"
+head_optics_start = "OpticsStart"
--- pylhc/machine_settings_info.py
+++ pylhc/machine_settings_info.py
@@ -4,32 +4,46 @@ Print Machine Settings Overview
 
 Prints an overview over the machine settings at a provided given time, or the current settings if
 no time is given.
+If an output path is given, all info will be written into tfs files,
+otherwise a summary is logged into console.
+
+Knob values can be extracted and the knob definition gathered. 
+For brevity reasons, this data is not logged into the summary in the console.
+If a start time is given, the trim history for the given knobs can be written out as well. 
+This data is also not logged.
 
 Can be run from command line, parameters as given in :meth:`pylhc.machine_settings_info.get_info`.
+All gathered data is returned, if this function is called from python.
 
 .. code-block:: none
 
-    usage: machine_settings_info.py [-h] [--time TIME] [--knobs KNOBS [KNOBS ...]]
-                                [--accel ACCEL] [--output_dir OUTPUT_DIR]
-                                [--knob_definitions] [--source SOURCE] [--log]
+    usage: machine_settings_info.py [-h] [--time TIME] [--start_time START_TIME]
+                                    [--knobs KNOBS [KNOBS ...]] [--accel ACCEL]
+                                    [--output_dir OUTPUT_DIR] [--knob_definitions]
+                                    [--source SOURCE] [--log]
 
     optional arguments:
-      -h, --help            show this help message and exit
-      --time TIME           UTC Time as 'Y-m-d H:M:S.f' format.
-      --knobs KNOBS [KNOBS ...]
+    -h, --help            show this help message and exit
+    --time TIME           UTC Time as 'Y-m-d H:M:S.f' format.
+                          Acts as point in time or end time
+                         (if ``start_time`` is given).
+    --start_time START_TIME
+                            UTC Time as 'Y-m-d H:M:S.f' format.
+                            Defines the beginning of the time-range.
+    --knobs KNOBS [KNOBS ...]
                             List of knobnames.
-      --accel ACCEL         Accelerator name.
-      --output_dir OUTPUT_DIR
+    --accel ACCEL         Accelerator name.
+    --output_dir OUTPUT_DIR
                             Output directory.
-      --source SOURCE       Source to extract data from.
-      --knob_definitions    Set to extract knob definitions.
-      --log                 Write summary into log (automatically done if no
-                            output path is given).
+    --knob_definitions    Set to extract knob definitions.
+    --source SOURCE       Source to extract data from.
+    --log                 Write summary into log
+                          (automatically done if no output path is given).
 
 
 :author: jdilly
 """
-from collections import OrderedDict
+from collections import OrderedDict, namedtuple
 
 import tfs
 from generic_parser import DotDict, EntryPointParameters, entrypoint
@@ -53,7 +67,15 @@ def _get_params() -> dict:
         time=dict(
             default=None,
             type=str,
-            help="UTC Time as 'Y-m-d H:M:S.f' format."),
+            help=("UTC Time as 'Y-m-d H:M:S.f' format."
+                  " Acts as point in time or end time (if ``start_time`` is given).")
+            ),
+        start_time=dict(
+            default=None,
+            type=str,
+            help=("UTC Time as 'Y-m-d H:M:S.f' format."
+                  " Defines the beginning of the time-range.")
+             ),
         knobs=dict(
             default=None,
             nargs="+",
@@ -81,62 +103,77 @@ def _get_params() -> dict:
 
 
 @entrypoint(_get_params(), strict=True)
-def get_info(opt) -> Tuple[AccDatetime, DotDict, DotDict, dict, dict]:
+def get_info(opt) -> Dict[str, object]:
     """
     Get info about **Beamprocess**, **Optics** and **Knobs** at given time.
 
     Keyword Args:
 
-    *--Optional--*
+       *--Optional--*
 
-    - **accel** *(str)*:
+        - **accel** *(str)*:
 
-        Accelerator name.
+            Accelerator name.
 
-        default: ``lhc``
-
-
-    - **knob_definitions**:
-
-        Set to extract knob definitions.
-
-        action: ``store_true``
+            default: ``lhc``
 
 
-    - **knobs** *(str)*:
+        - **knob_definitions**:
 
-        List of knobnames.
+            Set to extract knob definitions.
 
-        default: ``None``
-
-
-    - **log**:
-
-        Write summary into log (automatically done if no output path is
-        given).
-
-        action: ``store_true``
+            action: ``store_true``
 
 
-    - **output_dir** *(PathOrStr)*:
+        - **knobs** *(str)*:
 
-        Output directory.
+            List of knobnames.
 
-        default: ``None``
-
-
-    - **source** *(str)*:
-
-        Source to extract data from.
-
-        default: ``nxcals``
+            default: ``None``
 
 
-    - **time** *(str)*:
+        - **log**:
 
-        UTC Time as 'Y-m-d H:M:S.f' format.
+            Write summary into log (automatically done if no output path is
+            given).
 
-        default: ``None``
+            action: ``store_true``
+
+
+        - **output_dir** *(PathOrStr)*:
+
+            Output directory.
+
+            default: ``None``
+
+
+        - **source** *(str)*:
+
+            Source to extract data from.
+
+            default: ``nxcals``
+
+
+        - **start_time** *(str)*:
+
+            UTC Time as 'Y-m-d H:M:S.f' format. Defines the beginning of the time-
+            range.
+
+            default: ``None``
+
+
+        - **time** *(str)*:
+
+            UTC Time as 'Y-m-d H:M:S.f' format. Acts as point in time or end time
+            (if ``start_time`` is given).
+
+            default: ``None``
+
+    Returns:
+        dict: Dictionary containing the given ``time`` and ``start_time``,
+        the extracted ``beamprocess``-info and ``optics``-info, the
+        ``trim_histories`` and current (i.e. at given ``time``) ``trims``
+        and the ``knob_definitions``, if extracted.
 
     """
     if opt.output_dir is None:
@@ -144,29 +181,44 @@ def get_info(opt) -> Tuple[AccDatetime, DotDict, DotDict, dict, dict]:
 
     AccDT = AcceleratorDatetime[opt.accel]
     acc_time = AccDT.now() if opt.time is None else AccDT.from_utc_string(opt.time)
+    acc_start_time = None if opt.start_time is None else AccDT.from_utc_string(opt.start_time)
 
     beamprocess_info = _get_beamprocess(acc_time, opt.accel, opt.source)
 
-    optics_info, knob_definitions, trims = None, None, None
+    optics_info, knob_definitions, trim_histories = None, None, None
     try:
         optics_info = _get_optics(acc_time, beamprocess_info.Name, beamprocess_info.StartTime)
     except ValueError as e:
         LOG.error(str(e))
     else:
-        trims = LSA.find_trims_at_time(beamprocess_info.Object, opt.knobs, acc_time, opt.accel)
-        knobs_definitions = _get_knob_definitions(opt.knob_definitions, opt.knobs, optics_info.Name)
+        trim_histories = LSA.get_trim_history(beamprocess_info.Object, opt.knobs, start_time=acc_start_time, end_time=acc_time, accelerator=opt.accel)
+        trims = _get_last_trim(trim_histories)
+        if opt.knob_definitions:
+            knob_definitions = _get_knob_definitions(opt.knobs, optics_info.Name)
 
     if opt.log:
         log_summary(acc_time, beamprocess_info, optics_info, trims)
 
     if opt.output_dir is not None:
-        out = Path(opt.output_dir)
-        out.mkdir(parents=True, exist_ok=True)
-        write_summary(out, acc_time, beamprocess_info, optics_info, trims)
-        if knob_definitions is not None:
-            write_knob_defitions(out, knobs_definitions)
+        out_path = Path(opt.output_dir)
+        out_path.mkdir(parents=True, exist_ok=True)
+        write_summary(out_path, opt.accel, acc_time, beamprocess_info, optics_info, trims)
+        
+        if trim_histories and acc_start_time:
+            write_trim_histories(out_path, trim_histories, opt.accel, acc_time, acc_start_time, beamprocess_info, optics_info)
 
-    return acc_time, beamprocess_info, optics_info, trims, knobs_definitions
+        if knob_definitions:
+            write_knob_defitions(out_path, knob_definitions)
+
+    return {
+        "time": acc_time,
+        "start_time": acc_start_time,
+        "beamprocess": beamprocess_info,
+        "optics": optics_info,
+        "trim_histories": trim_histories,
+        "trims": trims,
+        "knob_definitions": knob_definitions
+    }
 
 
 # Output #######################################################################
@@ -207,13 +259,14 @@ def log_summary(acc_time: AccDatetime, bp_info: DotDict,
 
 
 def write_summary(
-    output_path: Path, acc_time: AccDatetime, bp_info: DotDict,
+    output_path: Path, accel: str, acc_time: AccDatetime, bp_info: DotDict,
     optics_info: DotDict = None, trims: Dict[str, float] = None
 ):
     """Write summary into a ``tfs`` file.
 
     Args:
         output_path (Path): Folder to write output file into
+        accel (str): Name of the accelerator
         acc_time (AccDatetime): User given Time
         bp_info (DotDict): BeamProcess Info Dictionary
         optics_info (DotDict): Optics Info Dictionary
@@ -225,6 +278,7 @@ def write_summary(
     info_tfs = tfs.TfsDataFrame(trims, columns=[const.column_knob, const.column_value])
     info_tfs.headers = OrderedDict([
         ("Hint:",                           "All times given in UTC."),
+        (const.head_accel,                  accel),
         (const.head_time,                   acc_time.cern_utc_string()),
         (const.head_beamprocess,            bp_info.Name),
         (const.head_fill,                   bp_info.Fill),
@@ -245,6 +299,66 @@ def write_knob_defitions(output_path: Path, definitions: dict):
     for knob, definition in definitions.items():
         path = output_path / f"{knob.replace('/', '_')}{const.knobdef_suffix}"
         tfs.write(path, definition, save_index=LSA_COLUMN_NAME)
+
+
+def write_trim_histories(
+    output_path: Path, trim_hisotries: Dict[str, namedtuple], accel: str,
+    acc_time: AccDatetime = None, acc_start_time: AccDatetime = None, 
+    bp_info: DotDict = None, optics_info: DotDict = None
+):
+    """ Write the trim histories into tfs files.
+    There are two time columns, one with timestamps as they are usually easier to handle
+    and one with the UTC-string, as they are more human readable.
+
+    Args:
+        output_path (Path): Folder to write output file into
+        trim_hisotries (dict): trims histories as extracted via LSA.get_trim_history()
+        accel (str): Name of the accelerator
+        acc_time (AccDatetime): User given (End)Time
+        acc_start_time (AccDatetime): User given Start Time
+        bp_info (DotDict): BeamProcess Info Dictionary
+        optics_info (DotDict): Optics Info Dictionary
+    """
+    AccDT = AcceleratorDatetime[accel]
+
+    # Create headers with basic info ---
+    headers = OrderedDict([("Hint:", "All times are given in UTC."), 
+                            (const.head_accel, accel)
+    ])
+    
+    if acc_start_time:
+        headers.update({const.head_start_time: acc_start_time.cern_utc_string()})
+
+    if acc_time:
+        headers.update({const.head_end_time: acc_time.cern_utc_string()})
+    
+    if bp_info:
+        headers.update({
+            const.head_beamprocess: bp_info.Name, 
+            const.head_fill: bp_info.Fill,
+        })
+
+    if optics_info:
+        headers.update({const.head_optics: optics_info.Name})
+
+    # Write trim history per knob ----
+    for knob, trim_history in trim_hisotries.items():
+        trims_tfs = tfs.TfsDataFrame(headers=headers, columns=[const.column_time, const.column_timestamp, const.column_value])
+        for timestamp, value in zip(trim_history.time, trim_history.data):
+            time = AccDT.from_timestamp(timestamp).cern_utc_string()
+            try:
+                len(value)
+            except TypeError:
+                # single value (as it should be)
+                trims_tfs.loc[len(trims_tfs), :] = (time, timestamp, value)
+            else:
+                # multiple values (probably weird)
+                LOG.debug("Multiple values in trim for {knob} at {time}.")
+                for item in value:
+                    trims_tfs.loc[len(trims_tfs), :] = (time, timestamp, item)
+
+        path = output_path / f"{knob.replace('/', '_')}{const.trimhistory_suffix}"
+        tfs.write(path, trims_tfs)
 
 
 # Beamprocess ##################################################################
@@ -305,16 +419,34 @@ def _get_last_optics(
 # Knobs ########################################################################
 
 
-def _get_knob_definitions(active: bool, knobs: list, optics: str):
-    """Get knob definitions if switch is activated."""
+def _get_knob_definitions(knobs: list, optics: str):
+    """Get knob definitions."""
     defs = {}
-    if active:
-        for knob in knobs:
-            try:
-                defs[knob] = LSA.get_knob_circuits(knob, optics)
-            except IOError as e:
-                LOG.warning(e.args[0])
+    for knob in knobs:
+        try:
+            defs[knob] = LSA.get_knob_circuits(knob, optics)
+        except IOError as e:
+            LOG.warning(e.args[0])
     return defs
+
+def _get_last_trim(trims: dict) -> dict:
+    """Returns the last trim in the trim history.
+
+    Args:
+        trims (dict): trim history as extracted via LSA.get_trim_history()
+
+    Returns:
+        Dictionary of knob names and their values.
+    """
+    trim_dict = {trim: trims[trim].data[-1] for trim in trims.keys()}  # return last set value
+    for trim, value in trim_dict.items():
+        try:
+            trim_dict[trim] = value.flatten()[-1]  # the very last entry ...
+        except AttributeError:
+            continue  # single value, as expected
+        else:
+            LOG.warning(f"Trim {trim} hat multiple data entries {value}, taking only the last one.")
+    return trim_dict
 
 
 # Script Mode ##################################################################
